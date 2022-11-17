9bd41c703b96b3866b22edebbd8da95259b646ac
Logan Snopek <LSnopek@lcs.on.ca>
2022-11-17 11:41:29 -0500

    Initial Commit

---
+++ ShapesExamples/ShapesExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesExamplesApp.swift
+//  ShapesExamples
+//
+//  Created by Logan Snopek on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct ShapesExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
